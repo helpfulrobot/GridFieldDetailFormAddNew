ed8c4e4467d1ee746ca312d800ff6f0ce7db14a4
helpfulrobot <chris@silverstripe.com>
2016-01-01 02:05:59 +1300

    Converted to PSR-2

--- code/extensions/GridFieldAddNewExtension.php
+++ code/extensions/GridFieldAddNewExtension.php
@@ -4,19 +4,20 @@
  * @author Ryan
  *
  */
-class GridFieldAddNewExtension extends DataExtension {
+class GridFieldAddNewExtension extends DataExtension
+{
     
     /**
      * adds a new button allowing users
-	 * to add a new record directly from the edit screen 
+     * to add a new record directly from the edit screen 
      * @param {Form} $form
      */
-    public function updateItemEditForm($form){       
+    public function updateItemEditForm($form)
+    {
         
         //check if this DataExtension should be used
-        if(Config::inst()->get('GridFieldAddNewExtension', 'use_data_extension')){
-        
-            if($this->owner->record->ID !== 0 && $this->owner->record->canCreate()){
+        if (Config::inst()->get('GridFieldAddNewExtension', 'use_data_extension')) {
+            if ($this->owner->record->ID !== 0 && $this->owner->record->canCreate()) {
                 $actionFields=$form->Actions();
             
                 $link = Controller::join_links($this->owner->gridField->Link('item'), 'new');
@@ -26,11 +27,7 @@ class GridFieldAddNewExtension extends DataExtension {
                                 'addNew',
                                 '<a href="' .$link. '" class="action action-detail ss-ui-action-constructive ss-ui-button ui-button ui-widget ui-state-default ui-corner-all new new-link" data-icon="add">' ._t("GridFieldDetailFormAddNew.ADD", "_Add"). ' '. $this->owner->record->i18n_singular_name(). '</a>')
                 );
-            
             }
         }
-               
     }
 }
-
-?>
